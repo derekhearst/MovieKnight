aedd36221e93097f698b50fcc12e01c98e3adb40
jon-cron <jonathanacm95@gmail.com>
2022-12-21 13:58:09 -0700

    making groups cards

--- MovieKnight.client/src/pages/HomePage.vue
+++ MovieKnight.client/src/pages/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-fluid">
-    <section class="row justify-content-center mt-3">
+    <section class="row justify-content-evenly mt-3">
       <!-- SECTION movies -->
-      <div class="col-7 m-2 p-1">
+      <div class="col-8 p-1 overflow-auto">
         <section class="row justify-content-evenly p-1">
           <!-- TODO v-for over this col-3 -->
           <div class="col-3 p-3" v-for="m in movies">
@@ -11,13 +11,23 @@
         </section>
       </div>
       <!-- SECTION groups and events -->
-      <div class="col-4">
+      <div class="col-3 border-style">
         <!-- SECTION groups -->
         <section class="row justify-content-center">
-          <div class="col-10">
+          <div class="col-10 d-flex justify-content-between">
             <h1 class="fw-bold">My Groups</h1>
+            <button class="btn maroon fw-bold fs-4">Make Group</button>
           </div>
-          <div class="col-10"></div>
+          <div class="col-10 card my-3">
+            <section class="row text-dark">
+              <div class="col-4">
+                <img src="http://thiscatdoesnotexist.com" class="img-fluid" alt="">
+              </div>
+              <div class="col-7">
+                <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magni fugit cum eveniet commodi suscipit! Ipsam dicta eveniet quo atque corrupti ipsa possimus at quaerat aliquam. Nostrum dolores nam quia.</h6>
+              </div>
+            </section>
+          </div>
         </section>
         <!-- SECTION events -->
         <section class="row">
@@ -60,5 +70,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.maroon{
+  background-color: #fbcf33;
+  color: #8f1515;
+}
+.border-style{
+  // border-color:#fbcf33;
+  border-left: 2px solid #fbcf33;
+}
 </style>
