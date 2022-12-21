6e48113ad535afd480fff44f94f2f6100d97fb9a
jon-cron <jonathanacm95@gmail.com>
2022-12-21 09:00:02 -0700

    ENV UPDATED

--- MovieKnight.client/src/env.js
+++ MovieKnight.client/src/env.js
@@ -1,6 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-0v0iohje0isldio2.us.auth0.com'
+export const clientId = 'lIY9KEl09Moz356G94mNTHWIfx3Erj5I'
+export const audience = 'https://classroom.com'
+
+
