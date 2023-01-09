69bb8e5f192baf23e5ef17b03aeae47ba098fcee
Derek Hearst <derekallenhearst@gmail.com>
2023-01-09 15:57:08 -0700

    updated enviorment

--- MovieKnight.client/src/env.js
+++ MovieKnight.client/src/env.js
@@ -1,8 +1,6 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const dev = window.location.origin.includes("localhost")
+export const baseURL = dev ? "http://localhost:3000" : "https://movieknight.uk"
 export const useSockets = false
-export const domain = 'dev-0v0iohje0isldio2.us.auth0.com'
-export const clientId = 'lIY9KEl09Moz356G94mNTHWIfx3Erj5I'
-export const audience = 'https://classroom.com'
-
-
+export const domain = "movieknight.us.auth0.com"
+export const clientId = "QSKSZe5bFQAYAR2nrlDDXbLxOWT9996Z"
+export const audience = "https://auth.movieknight.uk"
