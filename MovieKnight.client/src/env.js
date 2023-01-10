5a71ad884cef8a89ec4d3eaa2bdd99d6328c782a
Derek Hearst <derekallenhearst@gmail.com>
2023-01-10 09:53:47 -0700

    updated env again.

--- MovieKnight.client/src/env.js
+++ MovieKnight.client/src/env.js
@@ -2,5 +2,5 @@ export const dev = window.location.origin.includes("localhost")
 export const baseURL = dev ? "http://localhost:3000" : "https://api.movieknight.uk:3000"
 export const useSockets = false
 export const domain = "movieknight.us.auth0.com"
-export const clientId = "QSKSZe5bFQAYAR2nrlDDXbLxOWT9996Z"
+export const clientId = "pQnFxvcyv5bYlxaSIjwkQwUdzLoKxHlq"
 export const audience = "https://auth.movieknight.uk"
