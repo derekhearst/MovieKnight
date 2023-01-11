cf147d92dc1835f4bb80cb9b82521d525f887a74
Derek Hearst <derekallenhearst@gmail.com>
2023-01-11 11:07:56 -0700

    fixed comments

--- MovieKnight.client/src/AppState.js
+++ MovieKnight.client/src/AppState.js
@@ -2,7 +2,6 @@ import { reactive } from "vue"
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
-<<<<<<< HEAD
 	user: {},
 	/** @type {import('./models/Account.js').Account} */
 	account: {},
@@ -24,27 +23,4 @@ export const AppState = reactive({
 	activeEventMembers: [],
 	activeEventComments: [],
 	activeEventItems: [],
-=======
-  user: {},
-  /** @type {import('./models/Account.js').Account} */
-  account: {},
-  /** @type {import('./models/Movie.js').Movie[]} */
-  movies: [],
-  /** @type {import('./models/Movie.js').Movie[]} */
-  searchMovies: [],
-  /** @type {import('./models/Movie.js').Movie[]} */
-  activeMovie: {},
-  // /** @type {import('./models/Group.js').Group[]} */
-  myGroups: [],
-  activeGroup: null,
-  groupMovies: [],
-  activeComments: [],
-  activeGroupEvents: [],
-  activeEventId: {},
-  activeEvent: null,
-  activeEventMovie: [],
-  activeEventMembers: [],
-  myEvents: [],
-  groups: [],
->>>>>>> c461e7c9af15ff8e08b4756e4f27f34d6f08a5e4
 })
