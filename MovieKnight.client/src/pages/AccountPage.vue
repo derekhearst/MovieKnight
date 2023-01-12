3aa03fa579892c48cd971593d983ed1d4e5fa158
jon-cron <jonathanacm95@gmail.com>
2023-01-12 11:23:32 -0700

    working on account form

--- MovieKnight.client/src/pages/AccountPage.vue
+++ MovieKnight.client/src/pages/AccountPage.vue
@@ -1,9 +1,38 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-  </div>
+  <div class="container-fluid">
+        <div class="about text-center">
+          <h1>Welcome {{ account.name }}</h1>
+          <img class="rounded" :src="account.picture" alt="" />
+          <p>{{ account.email }}</p>
+          <p>{{ account.name }}</p>
+          <p>{{ account.picture }}</p>
+        </div>
+        <div class="about text-center">
+          <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          Button with data-target
+        </button>
+        <div class="collapse" id="collapseExample">
+          <div class="bg-dark text-center">
+            <form class="">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
