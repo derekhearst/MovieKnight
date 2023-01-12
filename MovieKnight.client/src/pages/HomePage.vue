c1165b2b024ce2fad1b2890242f313bfb067e7e1
Kendall Powell <kendalpowel@hotmail.com>
2023-01-11 19:22:15 -0700

    ?

--- MovieKnight.client/src/pages/HomePage.vue
+++ MovieKnight.client/src/pages/HomePage.vue
@@ -6,7 +6,7 @@
         <section class="row justify-content-evenly p-1">
           <!-- TODO v-for over this col-3 -->
           <div class="col-3 p-3" v-for="m in movies">
-            <MovieCard :movie="m"/>
+            <MovieCard :movie="m" />
           </div>
         </section>
       </div>
@@ -17,10 +17,11 @@
           <div class="col-10 d-flex justify-content-between">
             <h1 class="fw-bold">My Guilds</h1>
             <!-- TODO offcanvas form -->
-            <button v-if="account.id" class="btn maroon fw-bold fs-4"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">Form Guild</button>
+            <button v-if="account.id" class="btn maroon fw-bold fs-4" type="button" data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">Form Guild</button>
           </div>
           <div v-if="myGroups" v-for="g in myGroups" class="col-11">
-            <GroupCard :group="g.group"/>
+            <GroupCard :group="g.group" />
           </div>
         </section>
         <!-- SECTION events -->
@@ -45,67 +46,70 @@ import GroupCard from "../components/GroupCard.vue";
 
 
 export default {
-    setup() {
-        const editable = ref({});
-        const router = useRouter();
-        onMounted(() => {
-            getMovies();
-            // getMyGroups()
-        });
-        // async function getMyGroups(){
-        //   try {
-        //     await groupsService.getMyGroups()
-        //   } catch (error) {
-        //     Pop.error(error)
-        //     logger.log(error)
-        //   }
-        // }
-        async function getMovies() {
-            try {
-                await moviesService.getMovies();
-            }
-            catch (error) {
-                Pop.error(error);
-                logger.log(error);
-            }
+  setup() {
+    const editable = ref({});
+    const router = useRouter();
+    onMounted(() => {
+      getMovies();
+      // getMyGroups()
+    });
+    // async function getMyGroups(){
+    //   try {
+    //     await groupsService.getMyGroups()
+    //   } catch (error) {
+    //     Pop.error(error)
+    //     logger.log(error)
+    //   }
+    // }
+    async function getMovies() {
+      try {
+        await moviesService.getMovies();
+      }
+      catch (error) {
+        Pop.error(error);
+        logger.log(error);
+      }
+    }
+    return {
+      movies: computed(() => AppState.movies),
+      myGroups: computed(() => AppState.myGroups),
+      account: computed(() => AppState.account),
+      async createGroup() {
+        try {
+          await groupsService.createGroup(editable.value);
+          router.push({ name: "Group", params: { id: group.id } });
         }
-        return {
-            movies: computed(() => AppState.movies),
-            myGroups: computed(() => AppState.myGroups),
-            account: computed(()=> AppState.account),
-            async createGroup() {
-                try {
-                    await groupsService.createGroup(editable.value);
-                    router.push({ name: "Group", params: { id: group.id } });
-                }
-                catch (error) {
-                    Pop.error(error);
-                    logger.log(error);
-                }
-            }
-        };
-    },
-    // components: { GroupCard }
+        catch (error) {
+          Pop.error(error);
+          logger.log(error);
+        }
+      }
+    };
+  },
+  // components: { GroupCard }
 }
 </script>
 
 <style scoped lang="scss">
-.maroon{
+.maroon {
   background-color: #fbcf33;
   color: #8f1515;
 }
-.border-style{
+
+.border-style {
   // border-color:#fbcf33;
-  border-left: 2px solid #fbcf33;
+  border-left: 2px solid hsl(47, 96%, 59%);
 }
-.img-style{
+
+.img-style {
   height: 20vh;
   width: 40%;
   object-fit: cover;
   border-top-left-radius: 10px;
   border-bottom-left-radius: 10px;
 }
-.elevation-7{
+
+.elevation-7 {
   box-shadow: 3px 3px 3px #fbd033a2;
 }
 </style>
