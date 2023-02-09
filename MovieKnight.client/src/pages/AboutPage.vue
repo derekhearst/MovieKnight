f8b31ca9041286c2e79c7c21476d4fe0443e6ba6
Derek Hearst <derekallenhearst@gmail.com>
2023-02-09 10:12:10 -0700

    started about page

---
+++ MovieKnight.client/src/pages/AboutPage.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="homePage">
+		<article></article>
+	</div>
+</template>
+
+<style scoped>
+.homePage {
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	gap: 2rem;
+	min-height: 85vh;
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center;
+	background-color: rgba(0, 0, 0, 0.812);
+	background-blend-mode: overlay;
+	background-image: url('https://cdnb.artstation.com/p/assets/images/images/014/833/597/large/jeon-hyun-ho-26.jpg?1545797700');
+}
+</style>
--- MovieKnight.client/src/router.js
+++ MovieKnight.client/src/router.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHashHistory } from "vue-router"
-import { authGuard } from "@bcwdev/auth0provider-client"
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { authGuard } from '@bcwdev/auth0provider-client'
 
 function loadPage(page) {
 	return () => import(`./pages/${page}.vue`)
@@ -7,51 +7,56 @@ function loadPage(page) {
 
 const routes = [
 	{
-		path: "/",
-		name: "Home",
-		component: loadPage("HomePage"),
+		path: '/',
+		name: 'Home',
+		component: loadPage('HomePage'),
 	},
 	{
-		path: "/account",
-		name: "Account",
-		component: loadPage("AccountPage"),
+		path: '/account',
+		name: 'Account',
+		component: loadPage('AccountPage'),
 		beforeEnter: authGuard,
 	},
 	{
-		path: "/group/:id",
-		name: "Group",
-		component: loadPage("GroupPage"),
+		path: '/group/:id',
+		name: 'Group',
+		component: loadPage('GroupPage'),
 		beforeEnter: authGuard,
 	},
 	{
-		path: "/event/:id",
-		name: "Event",
-		component: loadPage("EventPage"),
+		path: '/event/:id',
+		name: 'Event',
+		component: loadPage('EventPage'),
 		beforeEnter: authGuard,
 	},
 	{
-		path: "/search/",
-		name: "Search",
-		component: loadPage("SearchPage"),
+		path: '/search/',
+		name: 'Search',
+		component: loadPage('SearchPage'),
 		beforeEnter: authGuard,
 	},
 	{
-		path: "/movieDetails/:id",
-		name: "MovieDetails",
-		component: loadPage("MovieDetailsPage"),
+		path: '/movieDetails/:id',
+		name: 'MovieDetails',
+		component: loadPage('MovieDetailsPage'),
 		beforeEnter: authGuard,
 	},
 	{
-		path: "/profile/:id",
-		name: "Profile",
-		component: loadPage("ProfilePage"),
+		path: '/profile/:id',
+		name: 'Profile',
+		component: loadPage('ProfilePage'),
 		beforeEnter: authGuard,
+	},
+	{
+		path: '/about/',
+		name: 'About',
+		component: loadPage('AboutPage'),
 	},
 ]
 
 export const router = createRouter({
-	linkActiveClass: "router-link-active",
-	linkExactActiveClass: "router-link-exact-active",
+	linkActiveClass: 'router-link-active',
+	linkExactActiveClass: 'router-link-exact-active',
 	history: createWebHashHistory(),
 	routes,
 })
