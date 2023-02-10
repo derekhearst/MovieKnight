7dc200aa256e2a1f287aceb59af2bd057b8f6418
Derek Hearst <derekallenhearst@gmail.com>
2023-02-10 09:36:45 -0700

    fixed adding group

--- MovieKnight.client/src/services/GroupsService.js
+++ MovieKnight.client/src/services/GroupsService.js
@@ -1,80 +1,78 @@
-import { AppState } from "../AppState.js"
+import { AppState } from '../AppState.js'
 // import { Group } from "../models/Group.js"
-import { logger } from "../utils/Logger.js"
-import Pop from "../utils/Pop.js"
-import { api } from "./AxiosService.js"
-import { eventsService } from "../services/EventsService.js"
-import { accountService } from "./AccountService.js"
+import { logger } from '../utils/Logger.js'
+import Pop from '../utils/Pop.js'
+import { api } from './AxiosService.js'
+import { eventsService } from '../services/EventsService.js'
+import { accountService } from './AccountService.js'
 
 class GroupsService {
 	async createGroup(body) {
-		const res = await api.post("api/groups", body)
+		const res = await api.post('api/groups', body)
 		this.getMyGroups()
 	}
 	async getMyGroups() {
-		const res = await api.get("account/groups")
-		let allGroups = res.data
-		AppState.myGroups = allGroups.filter(g => g.group.archived == true)
-
-		logger.log("Getting my groups", AppState.myGroups)
+		const res = await api.get(`/account/groups`)
+		AppState.myGroups = res.data
+		logger.log('Getting my groups', AppState.myGroups)
 	}
 	async getGroupById(id) {
-		const res = await api.get("api/groups/" + id)
+		const res = await api.get('api/groups/' + id)
 		AppState.activeGroup = res.data
-		logger.log("Getting group", res.data)
+		logger.log('Getting group', res.data)
 	}
 	async addMovieToGroup(groupId, movieData) {
 		delete movieData.id
 		const res = await api.post(`api/groups/${groupId}/movies`, movieData)
 		AppState.activeGroupMovies.push(res.data)
 		AppState.activeGroupMovies = AppState.activeGroupMovies
-		logger.log("Adding movie to group")
+		logger.log('Adding movie to group')
 	}
 	async getMoviesByGroupId(id) {
 		const res = await api.get(`api/groups/${id}/movies`)
 		AppState.activeGroupMovies = res.data
-		logger.log("Getting group movies", res.data)
+		logger.log('Getting group movies', res.data)
 	}
 	async postGroupComment(id, body) {
 		const res = await api.post(`api/groups/${id}/comments`, body)
 		// AppState.activeGroupComments.unshift(res.data)
 		AppState.activeGroupComments = AppState.activeGroupComments
-		logger.log("Posting group comment")
+		logger.log('Posting group comment')
 	}
 	async getCommentsByGroupId(id) {
 		const res = await api.get(`api/groups/${id}/comments`)
 		AppState.activeGroupComments = res.data.reverse()
-		logger.log("Getting group comments", res.data)
+		logger.log('Getting group comments', res.data)
 	}
 	async addMyselfToGroup(id) {
 		const res = await api.post(`api/groups/${id}/members`)
 		AppState.activeGroupMembers.push(res.data)
 		AppState.activeGroupMembers = AppState.activeGroupMembers
-		logger.log("Adding myself to group")
+		logger.log('Adding myself to group')
 	}
 	async removeMyselfFromGroup(id) {
 		let me = AppState.activeGroupMembers.find(m => m.accountId == AppState.account.id)
 		const res = await api.delete(`api/groups/${id}/members/${me.id}`)
 		AppState.activeGroupMembers = AppState.activeGroupMembers.filter(m => m.id != me.id)
-		logger.log("Removing myself from group")
+		logger.log('Removing myself from group')
 	}
 
 	async getGroupByGroupId(id) {
 		const res = await api.get(`api/groups/${id}`)
 		AppState.activeGroup = res.data
-		logger.log("Getting group", res.data)
+		logger.log('Getting group', res.data)
 	}
 
 	async getGroupMembersByGroupId(id) {
 		const res = await api.get(`api/groups/${id}/members`)
 		AppState.activeGroupMembers = res.data
-		logger.log("Getting group members", res.data)
+		logger.log('Getting group members', res.data)
 	}
 
 	async searchGroups(search) {
-		const res = await api.get("api/groups", { params: search })
+		const res = await api.get('api/groups', { params: search })
 		AppState.searchGroups = res.data
-		logger.log("Getting groups", res.data)
+		logger.log('Getting groups', res.data)
 	}
 	async archiveGroup(id) {
 		const res = await api.delete(`api/groups/${id}`)
